d7c63948d255fb88796bf26523c38b7c2ee9cd4a
Aarni Koskela <akx@iki.fi>
2021-05-28 10:18:27 +0300

    Pass client query string through to upstream unedited
    Refs #3

--- tests/test_asgiproxy.py
+++ tests/test_asgiproxy.py
@@ -1,10 +1,31 @@
 import pytest
 from asgiref.testing import ApplicationCommunicator
+from starlette.requests import Request
 
 from asgiproxy.context import ProxyContext
 from asgiproxy.proxies.http import proxy_http
 from tests.configs import ExampleComProxyConfig
 from tests.utils import http_response_from_asgi_messages, make_http_scope
+
+
+@pytest.mark.parametrize("full_url, expected_url", [
+    ("http://127.0.0.1/pathlet/?encode&flep&murp", "http://example.com/pathlet/?encode&flep&murp"),
+    ("http://127.0.0.1/pathlet/", "http://example.com/pathlet/"),
+])
+def test_query_string_passthrough(full_url, expected_url):
+    proxy_config = ExampleComProxyConfig()
+    scope = make_http_scope(
+        full_url=full_url,
+        headers={
+            "Accept": "text/html",
+            "User-Agent": "Foo",
+        },
+    )
+    client_request = Request(scope)
+    opts = proxy_config.get_upstream_http_options(
+        scope=scope, client_request=client_request, data=None
+    )
+    assert opts["url"] == expected_url
 
 
 @pytest.mark.asyncio
